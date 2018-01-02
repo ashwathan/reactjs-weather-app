93af5f5aa44f5168d9bc9cbf38fc61101afec706
Ashwath A N <ashwathsringeri@gmail.com>
2018-01-02 21:32:26 +0530

    Examples compoenent added

--- public/bundle.js
+++ public/bundle.js
@@ -58,6 +58,7 @@
 	var Main = __webpack_require__(216);
 	var Weather = __webpack_require__(218);
 	var About = __webpack_require__(219);
+	var Examples = __webpack_require__(220);
 
 	ReactDOM.render(React.createElement(
 	  Router,
@@ -66,6 +67,7 @@
 	    Route,
 	    { path: '/', components: Main },
 	    React.createElement(Route, { path: 'about', component: About }),
+	    React.createElement(Route, { path: 'examples', component: Examples }),
 	    React.createElement(IndexRoute, { component: Weather })
 	  )
 	), document.getElementById('app'));
@@ -24885,14 +24887,36 @@
 
 	var React = __webpack_require__(1);
 
+	var _require = __webpack_require__(159),
+	    Link = _require.Link;
+
 	var Nav = React.createClass({
 	    displayName: "Nav",
 
 	    render: function render() {
 	        return React.createElement(
-	            "h2",
+	            "div",
 	            null,
-	            "Nav component"
+	            React.createElement(
+	                "h2",
+	                null,
+	                "Nav component"
+	            ),
+	            React.createElement(
+	                Link,
+	                { to: "/" },
+	                "Get Weather"
+	            ),
+	            React.createElement(
+	                Link,
+	                { to: "/about" },
+	                "About"
+	            ),
+	            React.createElement(
+	                Link,
+	                { to: "/examples" },
+	                "Examples"
+	            )
 	        );
 	    }
 	});
@@ -24946,6 +24970,32 @@
 	});
 
 	module.exports = About;
+
+/***/ }),
+/* 220 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var Examples = React.createClass({
+	    displayName: 'Examples',
+
+	    render: function render() {
+	        return React.createElement(
+	            'div',
+	            null,
+	            React.createElement(
+	                'h2',
+	                null,
+	                'Examples Component'
+	            )
+	        );
+	    }
+	});
+
+	module.exports = Examples;
 
 /***/ })
 /******/ ]);
