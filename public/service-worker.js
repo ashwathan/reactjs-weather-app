f7916370acb573298bb404da9a90ae44b2cafbd7
Ashwath A N <ashwathsringeri@gmail.com>
2018-01-09 21:56:58 +0530

    Service worker testing

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+'use strict';
+importScripts('./sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'testcache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    'bundle.js',
+    'index.html',
+  ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;
