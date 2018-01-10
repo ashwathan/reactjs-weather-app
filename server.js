b267f02f31f43edc2cd85c79a3712f969e8c87df
Ashwath A N <ashwathsringeri@gmail.com>
2018-01-10 22:12:00 +0530

    gzip test

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000; 
 app.use(compress());
-app.use(function(req, res, next){
-  if(req.header['x-forwarded-proto'] === 'https'){
-    res.redirect('http://' + req.hostname + req.url);
-  } else{
-    next();
-  }
-});
+// app.use(function(req, res, next){
+//   if(req.header['x-forwarded-proto'] === 'https'){
+//     res.redirect('http://' + req.hostname + req.url);
+//   } else{
+//     next();
+//   }
+// });
 
 app.listen(PORT, function ( ) {
   console.log('Express server is up on port ' + PORT);
