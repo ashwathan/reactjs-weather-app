25aa5d845f8257450d72cb867aafdd8467a4c392
Ashwath A N <ashwathsringeri@gmail.com>
2018-01-11 20:08:16 +0530

    TEST gzip

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000; 
-//app.use(compress({filter: shouldCompress}))
+app.use(compress({filter: shouldCompress}))
 
 function shouldCompress (req, res) {
   if (req.headers['x-no-compression']) {
